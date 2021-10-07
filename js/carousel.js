75bf0ff681f4532ed2ec5407d324dc6f99ec8161
golibdev <gnarzullayev2000@gnail.com>
2021-10-07 10:59:35 +0500

    updated project

---
+++ js/carousel.js
@@ -0,0 +1,34 @@
+new Splide( '.splide' , {
+    type   : 'loop',
+    perMove: 1,
+    focus  : 'center',
+    autoWidth: true
+}).mount();
+
+new Splide( '.splides' , {
+    type   : 'loop',
+    perMove: 1,
+    focus  : 'center',
+    autoWidth: true,
+}).mount();
+
+new Splide( '.splide-teacher' , {
+    type   : 'loop',
+    perMove: 1,
+    focus  : 'center',
+    autoWidth: true,
+}).mount();
+
+new Splide( '.splide-results' , {
+    type   : 'loop',
+    perMove: 1,
+    focus  : 'center',
+    autoWidth: true,
+}).mount();
+
+new Splide( '.splide-comments' , {
+    type   : 'loop',
+    perMove: 1,
+    focus  : 'center',
+    autoWidth: true,
+}).mount();
